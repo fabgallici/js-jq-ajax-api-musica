aa98fbceedd4f5dcbc1d47039e7130bfeb366e83
cabal82 <fabriziogallici@gmail.com>
2019-11-19 14:45:35 +0100

    ajax work

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+/* Attraverso una chiamata ajax all’Api di boolean avremo 
+a disposizione una decina di dischi musicali.
+Servendoci di handlebars stampiamo tutto a schermo.
+ */
+
+function getMusic() {
+	$.ajax({
+		url: "https://flynn.boolean.careers/exercises/api/array/music",
+		method: "GET",
+		success: function (data) {
+			console.log('success', data);
+			var arrMusic = data.response;
+			evaluateMusicData(arrMusic);
+		},
+		error: function (error) {
+			console.log("error", error);
+		}
+	});
+}
+
+function evaluateMusicData(arrData) {
+	arrData.forEach(function (cd) {
+		// console.log('cd ', cd, "index ", index);
+		var cdPoster = cd.poster;
+		var cdTitle = cd.title;
+		console.log(cdPoster);
+	})
+}
+
+function printMusic(cdPoster, cdTitle, cdAuthor, cdYear) {
+	var source = document.getElementById('handle-addContact-template').innerHTML;
+	var musicTemplate = Handlebars.compile(source);
+	var musicData = { cdPoster: cdPoster, cdTitle: cdTitle, cdAuthor: cdAuthor, cdYear: cdYear };
+	var htmlMusicData = musicTemplate(musicData);
+	$('.cds-container.container').append(htmlMusicData);
+}
+
 $(document).ready(function() {
 	//Code
+	getMusic();
 });
